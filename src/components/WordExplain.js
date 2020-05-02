d757d0980bd605cf3089af27f072ffb9b2e81f3f
tachish <tachish256@gmail.com>
2020-05-02 13:13:49 +0800

    add explains finish half

--- src/components/WordExplain.js
+++ src/components/WordExplain.js
@@ -1,18 +1,42 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import classnames from 'classnames'
 
 function WordExplain(props) {
-
+//classnames
     const explainContainer = classnames('explainContainer')
     const wordExplain = classnames("wordExplain")
     const addExplain = classnames("addExplain")
     const meaning = classnames('meaning')
     const setCollection = classnames('setCollection')
-    const indentation = classnames('indentation')
-
+//const
     const explainPlaceHolder = "Meaning"
     const setCollectionPlaceHolder = "Set Collection"
-
+//states
+    const [explains, setexplains] = useState(['',''])
+//innet handler
+    const addMeaning = (e, explainContainerRef) => {
+        e.nativeEvent.stopImmediatePropagation();
+        console.log('add meaning', explainContainerRef);
+        //explains.push(setCollectionInit)
+    }
+    const addSetCollection = (e) => {
+        e.nativeEvent.stopImmediatePropagation();
+        setexplains([...explains,''])
+    }
+    const handelSetColectionChange = (e, index) => {
+        console.log('onchange is called');
+        let target = e.target.value;
+        setexplains(explains.map((item,i) => {
+            if (i === index) {
+                item = target
+            }
+            console.log('item is ', item);
+            console.log('index is ', index);
+            console.log(explains)
+            return item
+        }))
+    }
+//inner jsx obj
     const Meaning = (
         <div className={wordExplain}>
             <div
@@ -28,29 +52,25 @@ function WordExplain(props) {
     )
     const Explain = (
         <ul>
-            <li className={wordExplain}>
-                <div
-                    className={addExplain}
-                    onClick={(e) => { addSetCollection(e) }}
-                >+</div>
-                <input
-                    type="text"
-                    className={setCollection}
-                    placeholder={setCollectionPlaceHolder}
-                />
-            </li>
+            {explains.map((item, index) => {
+                return (
+                    <li className={wordExplain} key={index}>
+                        <div
+                            className={addExplain}
+                            onClick={(e) => { addSetCollection(e) }}
+                        >+</div>
+                        <input
+                            type="text"
+                            className={setCollection}
+                            placeholder={setCollectionPlaceHolder}
+                            value={item}
+                            onChange={(e)=>{handelSetColectionChange(e,index)}}
+                        />
+                    </li>
+                )
+            })}
         </ul>
     )
-
-    const addMeaning = (e, explainContainerRef) => {
-        e.nativeEvent.stopImmediatePropagation();
-        console.log('add meaning', explainContainerRef);
-
-    }
-    const addSetCollection = (e) => {
-        e.nativeEvent.stopImmediatePropagation();
-        console.log("add set collection");
-    }
     return (
         <ul
             className={explainContainer}
